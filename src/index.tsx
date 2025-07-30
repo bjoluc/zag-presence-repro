0ef11858075016f989846fe8af610e86933d6e4b
Björn Luchterhandt <bjoern.luchterhandt@kilanka.de>
2025-07-30 17:40:52 +0200

    Add repro

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,76 @@
 /* @refresh reload */
-import { render } from 'solid-js/web';
 
-import App from './App';
+import * as presence from "@zag-js/presence";
+import { normalizeProps, useMachine } from "@zag-js/solid";
+import {
+	type Component,
+	createMemo,
+	createSignal,
+	type ParentComponent,
+	splitProps,
+} from "solid-js";
+import { render, Show } from "solid-js/web";
 
-const root = document.getElementById('root');
+const Presence: ParentComponent<{
+	present: boolean;
+	unmountOnExit?: boolean;
+	onExitComplete?: () => void;
+}> = (props) => {
+	const [machineProps, localProps] = splitProps(props, [
+		"present",
+		"unmountOnExit",
+		"onExitComplete",
+	]);
 
-if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
-  throw new Error(
-    'Root element not found. Did you forget to add it to your index.html? Or maybe the id attribute got misspelled?',
-  );
-}
+	const service = useMachine(presence.machine, machineProps);
 
-render(() => <App />, root!);
+	const api = createMemo(() => presence.connect(service, normalizeProps));
+	const unmount = createMemo(
+		() => !api().present && machineProps.unmountOnExit,
+	);
+
+	return (
+		<Show when={!unmount()}>
+			<div
+				hidden={!api().present}
+				data-state={api().skip ? undefined : api().present ? "open" : "closed"}
+				// This works:
+				// ref={api().setNode}
+
+				// This does not work:
+				ref={(ref) => {
+					api().setNode(ref);
+				}}
+				style={{
+					animation: `fade-${machineProps.present ? "in" : "out"} 1s forwards`,
+				}}
+				{...localProps}
+			/>
+		</Show>
+	);
+};
+
+const Root: Component = () => {
+	const [isPresent, setIsPresent] = createSignal(false);
+	return (
+		<>
+			<style>
+				{`
+				@keyframes fade-in { from { opacity: 0; } to { opacity: 1; } }
+				@keyframes fade-out { from { opacity: 1; } to { opacity: 0; } }
+				`}
+			</style>
+			<button
+				type="button"
+				onClick={() => {
+					setIsPresent(!isPresent());
+				}}
+			>
+				Toggle presence
+			</button>
+			<Presence present={isPresent()}>Children</Presence>
+		</>
+	);
+};
+
+render(() => <Root />, document.getElementById("root"));
